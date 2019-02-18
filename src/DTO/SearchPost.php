61cea6a95b13d2f2ff438262326410ba40394533
pastuszek666 <47759140+pastuszek666@users.noreply.github.com>
2019-02-18 22:15:09 +0100

    Create SearchPost.php

---
+++ src/DTO/SearchPost.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SearchPost {
+       
+    /**
+     * @Assert\Length(
+     *      max = 120
+     * )
+     */
+    public $search;
+    
+    /**
+     *   
+     *     
+     * 
+     */
+    public $category;
+    
+    public $status;
+    
+    public $tags;
+    
+
+   
+//    public function __construct(string $search, Category $category)
+//    {
+//        $this->search = $search;
+//        $this->category = $category;
+//    }
+
+   
+    /**
+     * Get search
+     *
+     * @return string search
+     */
+    public function getSearch(): ?string
+    {
+        return $this->search;
+    }
+    
+    /**
+     * Get category
+     *
+     * @return App\Entity\Category 
+     */
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+}
